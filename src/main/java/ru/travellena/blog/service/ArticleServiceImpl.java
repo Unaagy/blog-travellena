3120a26b4a7c9c58415ebf028d620bcee5e5b43c
Tuchnyak <georgii.o.sv@gmail.com>
2018-09-30 00:17:03 +0300

    Add Service implementation

---
+++ src/main/java/ru/travellena/blog/service/ArticleServiceImpl.java
@@ -0,0 +1,25 @@
+package ru.travellena.blog.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import ru.travellena.blog.dao.ArticleDao;
+import ru.travellena.blog.entity.Article;
+
+@Service
+public class ArticleServiceImpl implements ArticleService {
+
+	@Autowired
+	@Qualifier("articleDaoImplTest")
+	private ArticleDao dao;
+	
+	@Override
+	public List<Article> getAllArticles() {
+		
+		return dao.getAllArticles();
+	}
+
+}
